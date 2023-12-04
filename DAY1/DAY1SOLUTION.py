e234b9dd0bbc8520a1e307107329bf5fe26e78dc
Paul Racisz <paulr@fireflyint.com>
2023-12-04 12:15:54 -0500

    solved day 1

--- DAY1/DAY1SOLUTION.py
+++ DAY1/DAY1SOLUTION.py
@@ -1,18 +1,34 @@
+# import regex
 import re
 
+# import the input file
 file_path = 'puzzleinput.txt'
 
-calibrated_numbers_array = []
+# total count of digit numbers
 total_count = 0
 
+# open the file, read only
 with open(file_path, 'r') as file:
+    # iterate over each line in the file
     for line in file:
-        first_number = re.search("[1-9]", line)
+        # regex group that captures the first digit in a given string
+        first_number = re.search("\d", line)
+        # regex group that captures the last digit in a given string
         last_number = re.search("(\d+)(?!.*\d)", line)
-        finalNumber = first_number.group() + last_number.group()
+        # last_number can sometimes double up, so if string is greater than 1, grab the last digit in string
+        if len(last_number.group()) > 1:
+            # slice last digit off to avoid duplicate numbers
+            last_number = last_number.group()[-1]
+            # put two digits together ex: ("4" and "5" becomes "45")
+            finalNumber = first_number.group() + last_number
+        else:
+        # is the last_number is not doubled up, just add the groups (str values) together
+            finalNumber = first_number.group() + last_number.group()
+        # convert it into an int for adding to the final number
         convertedFinalNumber = int(finalNumber)
         print("this is the line: ", line,  " this is the converted final number:", convertedFinalNumber)
+        # add them all together
         total_count += convertedFinalNumber
-        calibrated_numbers_array.append(finalNumber)
 
+# print the total count to find the answer
 print(total_count)
